7efb4ca3c9aafc9d5a38c1f8ccb3f54becc4878f
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-03-27 16:02:38 +0530

    Added more features on bookingReview

--- UberReviewService/src/main/java/org/example/uberreviewservice/models/Review.java
+++ UberReviewService/src/main/java/org/example/uberreviewservice/models/Review.java
@@ -1,11 +1,37 @@
 package org.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.util.Date;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "bookingReview")
 public class Review {
 
     @Id  // this annotations makes the id property a primary key of our table
-    long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Double ratings;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
 }
---
+++ UberReviewService/src/main/java/org/example/uberreviewservice/repositories/ReviewRepository.java
@@ -0,0 +1,4 @@
+package org.example.uberreviewservice.repositories;
+
+public class ReviewRepository {
+}
