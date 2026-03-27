94ae7a92cd2fc865212a58b6479eee7db40b2e85
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-03-27 17:25:38 +0530

    Added Review repository usng JPARepository

--- UberReviewService/src/main/java/org/example/uberreviewservice/models/Review.java
+++ UberReviewService/src/main/java/org/example/uberreviewservice/models/Review.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
@@ -12,6 +13,7 @@ import java.util.Date;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Table(name = "bookingReview")
 public class Review {
---
+++ UberReviewService/src/main/java/org/example/uberreviewservice/services/ReviewService.java
@@ -0,0 +1,40 @@
+package org.example.uberreviewservice.services;
+
+import org.example.uberreviewservice.models.Review;
+import org.example.uberreviewservice.repositories.ReviewRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ReviewService implements CommandLineRunner {
+
+    ReviewRepository reviewRepository;
+
+    public ReviewService(ReviewRepository reviewRepository){
+        this.reviewRepository = reviewRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.print("************");
+
+        Review r= Review.
+                   builder().
+                   content("Best quality ride").
+                   ratings(5.0).
+                   build();
+
+        reviewRepository.save(r); // this code executes sql query
+
+        List<Review> reviews = reviewRepository.findAll();
+
+        for(Review review : reviews){
+            System.out.println(review.getContent());
+        }
+    }
+
+
+}
