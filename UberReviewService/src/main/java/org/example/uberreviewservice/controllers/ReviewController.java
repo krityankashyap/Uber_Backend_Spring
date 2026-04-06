f9290244aebd4c683c263ca055845e4f83ea97e9
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-04-06 11:30:16 +0530

    Added ReviewController layer

--- UberReviewService/src/main/java/org/example/uberreviewservice/controllers/ReviewController.java
+++ UberReviewService/src/main/java/org/example/uberreviewservice/controllers/ReviewController.java
@@ -5,10 +5,10 @@ import org.example.uberreviewservice.services.ReviewService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/review")
@@ -26,4 +26,46 @@ public class ReviewController {
         Review review = this.reviewService.publishReview(request);
         return new ResponseEntity<>(review , HttpStatus.CREATED);
     }
-}
+
+    @GetMapping
+    public ResponseEntity<List<Review>> getAllReviews(){
+        List<Review> reviews= this.reviewService.findAllReview();
+        return new ResponseEntity<>(reviews, HttpStatus.OK);
+    }
+
+    @GetMapping("/{reviewId}")
+    public ResponseEntity<?> findReviewById(@PathVariable Long reviewId){
+        try{
+            Optional<Review> review= this.reviewService.findReviewById(reviewId);
+            return new ResponseEntity<>(review, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<?> deleteReviewById(@PathVariable Long reviewId) {
+        try {
+            boolean isDeleted = this.reviewService.DeleteReviewById(reviewId);
+            if (!isDeleted) {
+                return new ResponseEntity<>("Unable to delete the review", HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+            return new ResponseEntity<>("Review deleted successfully", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+        @PutMapping("/{reviewId}")
+        public ResponseEntity<?> updateReview(@PathVariable Long reviewId, @RequestBody Review request){
+            try {
+                Review review = this.reviewService.updateReview(reviewId, request);
+                return new ResponseEntity<>(review, HttpStatus.OK);
+            }catch (Exception e){
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+
+
+
