b7c1ede3ec1118729068c52c16175138d7687eab
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-04-15 17:02:27 +0530

    Added AuthenticationProvider and AuthenticationManager of spring security

--- UberProject-AuthService/src/main/java/org/example/uberprojectauthservice/configurations/SpringSecurity.java
+++ UberProject-AuthService/src/main/java/org/example/uberprojectauthservice/configurations/SpringSecurity.java
@@ -1,10 +1,17 @@
 package org.example.uberprojectauthservice.configurations;
 
+import org.example.uberprojectauthservice.service.UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +19,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SpringSecurity {
 
     @Bean
+     public UserDetailsService userDetailsService(){
+         return new UserServiceImpl();
+     }
+
+    @Bean
+
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity.csrf(csrf->csrf.disable())
                 .authorizeHttpRequests(auth ->
@@ -19,6 +32,23 @@ public class SpringSecurity {
                 requestMatchers("/api/v1/auth/signup/*").permitAll().
                 requestMatchers("/api/v1/auth/signin/*").permitAll())
                 .build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider= new DaoAuthenticationProvider(userDetailsService());
+               authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+        return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
--- UberProject-AuthService/src/main/java/org/example/uberprojectauthservice/service/UserServiceImpl.java
+++ UberProject-AuthService/src/main/java/org/example/uberprojectauthservice/service/UserServiceImpl.java
@@ -3,6 +3,7 @@ package org.example.uberprojectauthservice.service;
 import org.example.uberprojectauthservice.Helpers.AuthPassengerDetails;
 import org.example.uberprojectauthservice.models.Passenger;
 import org.example.uberprojectauthservice.repositories.PassengerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,15 +12,15 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 /**
- * This class is responsible for loading the user in the form of UserDetails for the auth 
+ * This class is responsible for loading the user in the form of UserDetails for the auth
  */
 @Service
 public class UserServiceImpl implements UserDetailsService {
-    private final PassengerRepository passengerRepository;
 
-    public UserServiceImpl(PassengerRepository passengerRepository) {
-        this.passengerRepository = passengerRepository;
-    }
+    @Autowired
+    private  PassengerRepository passengerRepository;
+
+
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
